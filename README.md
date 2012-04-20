81d4c8dca362f8a0465e0fdee3ab7543f578752a
Martin Kleppe <kleppe@ubilabs.net>
2012-04-20 23:15:32 +0200

    Code clean up.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Mobile Range Slider 
+## A Touch Slider for Webkit / Mobile Safari
+
+https://github.com/ubilabs/mobile-range-slider
+
+Developed by [Ubilabs](http://ubilabs.net)
--- mobile-range-slider.js
+++ mobile-range-slider.js
@@ -1,32 +1,33 @@
 /*
- * This is a total rewrite of this plugin, done by Ubilabs GmbH
+ * Mobile Range Slider 
+ * A Touch Slider for Webkit / Mobile Safari
  *
- * Find more about this plugin by visiting
- * http://miniapps.co.uk/
+ * https://github.com/ubilabs/mobile-range-slider
  *
- * Copyright (c) 2010 - 2012 Alex Gibson, http://miniapps.co.uk/
  * Released under MIT license 
  * http://miniapps.co.uk/license/
- * 
+ * 2012 Ubilabs, http://ubilabs.net
  */
 
- 
+// function.bind() polyfill
+// taken from: https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Function/bind#Compatibility
 if (!Function.prototype.bind) {
   Function.prototype.bind = function (oThis) {
     if (typeof this !== "function") {
-      // closest thing possible to the ECMAScript 5 internal IsCallable function
-      throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+      throw new TypeError(
+        "Function.prototype.bind - what is trying to be bound is not callable"
+      );
     }
 
     var aArgs = Array.prototype.slice.call(arguments, 1), 
-        fToBind = this, 
-        fNOP = function () {},
-        fBound = function () {
-          return fToBind.apply(this instanceof fNOP
-                                 ? this
-                                 : oThis || window,
-                               aArgs.concat(Array.prototype.slice.call(arguments)));
-        };
+      fToBind = this, 
+      fNOP = function() { },
+      fBound = function() {
+        return fToBind.apply(
+          this instanceof fNOP ? this : oThis || window,
+          aArgs.concat( Array.prototype.slice.call(arguments) )
+        );
+      };
 
     fNOP.prototype = this.prototype;
     fBound.prototype = new fNOP();
@@ -34,14 +35,18 @@ if (!Function.prototype.bind) {
     return fBound;
   };
 }
- 
+
+
 (function(undefined) {
+  
+  // mapping of event handlers
   var events = {
     start: ['touchstart', 'mousedown'],
     move: ['touchmove', 'mousemove'],
     end: ['touchend', 'touchcancel', 'mouseup']
   };
 
+  // constructor
   function MobileRangeSlider(element, options) {
 
     this.element = element;
@@ -54,55 +59,51 @@ if (!Function.prototype.bind) {
     
     for (property in this.defaultOptions){
       if (options[property] !== undefined){
-         this.options[property] = options[property];
+        // set options passed to constructor
+        this.options[property] = options[property];
       } else {
+        // set default options
         this.options[property] = this.defaultOptions[property];
       }
     }
  
+    // detect support for Webkit CSS 3d transforms
+    this.supportsWebkit3dTransform = (
+      'WebKitCSSMatrix' in window && 
+      'm11' in new WebKitCSSMatrix()
+    );
+    
+    // store references to DOM elements
     if (typeof element === 'string'){
       this.element = document.getElementById(element);
     }
-    
-    //detect support for Webkit CSS 3d transforms
-    this.supportsWebkit3dTransform = ('WebKitCSSMatrix' in window && 'm11' in new WebKitCSSMatrix());
+        
     this.knob = this.element.getElementsByClassName('knob')[0];
     this.track = this.element.getElementsByClassName('track')[0];
     
+    // set context for event handlers
     this.start = this.start.bind(this);
     this.move = this.move.bind(this);
     this.end = this.end.bind(this);
     
+    // set the inital value
     this.addEvents("start");
     this.setValue(this.options.value);
     
-    
-    window.addEventListener("resize", (function(){
-      this.setValue(this.value);
-    }).bind(this));    
+    // update postion on page resize
+    window.addEventListener("resize", this.update.bind(this));
   }
   
+  // default options
   MobileRangeSlider.prototype.defaultOptions = {
-    value: 0,
-    min: 0,
-    max: 100,
-    callback: null
+    value: 0, // initial value
+    min: 0, // minimum value
+    max: 100, // maximum value
+    callback: null // change callback
   };
-  
-  MobileRangeSlider.prototype.handle = function(event){
-    
-    event.preventDefault();
-    
-    if (event.targetTouches){
-      event = event.targetTouches[0];
-    }
-    
-    this.moveKnobTo(event.pageX);
-  };
-  
-  
+
+  // add event handlers for a given name
   MobileRangeSlider.prototype.addEvents = function(name){
-    
     var list = events[name], 
       handler = this[name],
       all;
@@ -112,6 +113,7 @@ if (!Function.prototype.bind) {
     }
   };
   
+  // remove event handlers for a given name
   MobileRangeSlider.prototype.removeEvents = function(name){ 
     var list = events[name], 
       handler = this[name],
@@ -122,53 +124,70 @@ if (!Function.prototype.bind) {
     }
   };
   
+  // start to listen for move events
   MobileRangeSlider.prototype.start = function(event) {
     this.addEvents("move");
     this.addEvents("end");
     this.handle(event);
   };
   
+  // handle move events
   MobileRangeSlider.prototype.move = function(event) {
     this.handle(event);
   }; 
 
+  // stop listening for move events
   MobileRangeSlider.prototype.end = function() {
     this.removeEvents("move");
     this.removeEvents("end");
   };
   
+  // update the knob position
+  MobileRangeSlider.prototype.update = function() {
+    this.setValue(this.value);
+  };
+  
+  // set the new value of the slider
   MobileRangeSlider.prototype.setValue = function(value) {
     
-    if (value === undefined){
-      value = this.options.min;
-    }
+    if (value === undefined){ value = this.options.min; }
+    
+    value = Math.min(value, this.options.max);
+    value = Math.max(value, this.options.min);
     
     var 
       knobWidth = this.knob.offsetWidth,
       trackWidth = this.track.offsetWidth,
       range = this.options.max - this.options.min,
       width = trackWidth - knobWidth,
-      pos = Math.round((value - this.options.min) * width / range);
+      position = Math.round((value - this.options.min) * width / range);
     
-    //use Webkit CSS 3d transforms for hardware acceleration if available 
+    this.setKnobPosition(position);
+    
+    this.value = value;
+    this.callback(value);
+  };
+  
+  MobileRangeSlider.prototype.setKnobPosition = function(x){
+    // use Webkit CSS 3d transforms for hardware acceleration if available 
     if (this.supportsWebkit3dTransform) {
-      this.knob.style.webkitTransform = 'translate3d(' + pos + 'px, 0, 0)';
+      this.knob.style.webkitTransform = 'translate3d(' + x + 'px, 0, 0)';
     } else {
       this.knob.style.webkitTransform = 
       this.knob.style.MozTransform = 
       this.knob.style.msTransform = 
       this.knob.style.OTransform = 
-      this.knob.style.transform = 'translateX(' + pos + 'px)';
+      this.knob.style.transform = 'translateX(' + x + 'px)';
     }
-    
-    this.value = value;
-    this.callback(value);
   };
 
-  //moves the slider
-  MobileRangeSlider.prototype.moveKnobTo = function(pos) {
-
-    var element,
+  // handle a mouse event
+  MobileRangeSlider.prototype.handle = function(event){
+    event.preventDefault();
+    if (event.targetTouches){ event = event.targetTouches[0]; }
+  
+    var position = event.pageX, 
+      element,
       knobWidth = this.knob.offsetWidth,
       trackWidth = this.track.offsetWidth,
       width = trackWidth - knobWidth,
@@ -176,33 +195,23 @@ if (!Function.prototype.bind) {
       value;
       
     for (element = this.element; element; element = element.offsetParent){
-      pos -= element.offsetLeft;
+      position -= element.offsetLeft;
     }
-
-    pos += knobWidth / 2;
-    pos = Math.min(pos, trackWidth);
-    pos = Math.max(pos - knobWidth, 0);
+    
+    // keep knob in the bounds
+    position += knobWidth / 2;
+    position = Math.min(position, trackWidth);
+    position = Math.max(position - knobWidth, 0);
   
-    //use Webkit CSS 3d transforms for hardware acceleration if available 
-    if (this.supportsWebkit3dTransform) {
-      this.knob.style.webkitTransform = 'translate3d(' + pos + 'px, 0, 0)';
-    } else {
-      this.knob.style.webkitTransform = 
-      this.knob.style.MozTransform = 
-      this.knob.style.msTransform = 
-      this.knob.style.OTransform = 
-      this.knob.style.transform = 'translateX(' + pos + 'px)';
-    }
+    this.setKnobPosition(position);
   
-    //return value change as a percentage
-    value = this.options.min + Math.round(pos  * range / width );
-        
+    // update
+    value = this.options.min + Math.round(position * range / width);
     this.setValue(value);
   };
 
-  //callback method will be implemented by user
+  // call callback with new value
   MobileRangeSlider.prototype.callback = function(value) { 
-    
     if (this.options.callback){
       this.options.callback(value);
     }
@@ -210,5 +219,4 @@ if (!Function.prototype.bind) {
 
   //public function
   window.MobileRangeSlider = MobileRangeSlider;
-  
 })();
